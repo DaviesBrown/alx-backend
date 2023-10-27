8e6f387439c6f23867da6a04abe10a223e2a0940
Davies Brown <nwosudavid77@gmail.com>
2023-10-27 21:14:42 +0100

    fifo caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,20 +11,20 @@ class BasicCache(BaseCaching):
         """initialize
         """
         super().__init__()
-    
+
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key == None:
+        if key is None:
             return
-        if item == None:
+        if item is None:
             return
         self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Get an item by key
         """
-        if key == None:
+        if key is None:
             pass
         if key not in self.cache_data.keys():
             return None
