bf21a8009c79a36402971e84297ebcf73ae01fae
Davies Brown <nwosudavid77@gmail.com>
2023-10-28 02:23:18 +0100

    mru caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -3,30 +3,38 @@
 mru caching module
 """
 BaseCaching = __import__('base_caching').BaseCaching
-from base_caching import BaseCaching
+
 
 class MRUCache(BaseCaching):
     """ Most Recently Used Cache class"""
-    def __init__(self) -> None:
+
+    def __init__(self):
         """initialize
         """
         super().__init__()
-    
+        self.keys = []
+
     def put(self, key, item):
-        if key is None or item is None:
-            return
-
-        # Check if the cache is full
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            mru_key = list(self.cache_data.keys())[-1]  # Get the MRU key
-            del self.cache_data[mru_key]
-            print(f"DISCARD: {mru_key}\n")
-
-        # Add the new item to the cache
-        self.cache_data[key] = item
+        """ Add an item in the cache
+        """
+        if (key and item):
+            self.cache_data[key] = item
+            if key in self.keys:
+                self.keys.remove(key)
+                self.keys.append(key)
+            else:
+                self.keys.append(key)
+            if len(self.keys) > self.MAX_ITEMS:
+                idx = self.MAX_ITEMS - 1
+                erase = self.keys.pop(idx)
+                del self.cache_data[erase]
+                print('DISCARD: {}'.format(erase))
 
     def get(self, key):
+        """ Get an item by key
+        """
         if key is None or key not in self.cache_data:
             return None
-
+        self.keys.remove(key)
+        self.keys.append(key)
         return self.cache_data[key]
