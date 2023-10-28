df28433fe3b85219e1aa6e0ee91d561d46c911ed
Davies Brown <nwosudavid77@gmail.com>
2023-10-28 02:50:10 +0100

    lfu caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,33 +3,57 @@
 lfu caching module
 """
 BaseCaching = __import__('base_caching').BaseCaching
-from base_caching import BaseCaching
+
 
 class LFUCache(BaseCaching):
     """ Least Frequently Used Cache class"""
-    def __init__(self) -> None:
-        """initialize
-        """
+    def __init__(self):
+        '''
+        Initialize
+        '''
         super().__init__()
-    
+        self.keys = []
+        self.counter = {}
+
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key == None or item == None:
-            pass
-        if key in self.cache_data:
-            print('jee')
+        '''
+        Method that puts a new key-value pair to the cache
+        '''
+
+        if key is not None and item is not None:
+            if (len(self.keys) == BaseCaching.MAX_ITEMS and
+                    key not in self.keys):
+                discard = self.keys.pop(self.keys.index(self.findLFU()))
+                del self.cache_data[discard]
+                del self.counter[discard]
+                print('DISCARD: {:s}'.format(discard))
             self.cache_data[key] = item
-        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
-            key_discarded = list(self.cache_data.keys())[0]
-            self.cache_data.pop(key_discarded)
-            print(self.cache_data)
-            print("DISCARD: {}\n".format(key_discarded))
-        self.cache_data[key] = item
-    
+            if key not in self.keys:
+                self.keys.append(key)
+                self.counter[key] = 0
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+                self.counter[key] += 1
+
     def get(self, key):
-        """ Get an item by key
-        """
-        if key == None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data[key]
+        '''
+        Method that retrieves the item associated with key
+        '''
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            self.counter[key] += 1
+            return self.cache_data[key]
+        return None
+
+    def findLFU(self):
+        '''
+        Method that returns the last frequently used element
+        '''
+        items = list(self.counter.items())
+        freqs = [item[1] for item in items]
+        least = min(freqs)
+
+        lfus = [item[0] for item in items if item[1] == least]
+        for key in self.keys:
+            if key in lfus:
+                return key
