d9fe029afe85a7e55326d4aa9da418cc26436a94
Davies Brown <nwosudavid77@gmail.com>
2023-11-03 04:49:11 +0100

    get locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,7 +4,6 @@ flask app
 """
 from flask import Flask, g, render_template, request
 from flask_babel import Babel
-import pytz
 
 
 flask_app = Flask(__name__)
@@ -14,7 +13,7 @@ babel = Babel(flask_app)
 class Config(object):
     """ config class"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = babel.default_locale
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = babel.default_timezone.zone
 
 
@@ -22,13 +21,15 @@ flask_app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
-    """ get locale"""
+def get_locale() -> str:
+    """ get locale
+        returns best match for locale
+    """
     return request.accept_languages.best_match(flask_app.config['LANGUAGES'])
 
 
 @flask_app.route("/", strict_slashes=False)
-def index():
+def index() -> str:
     """ index route"""
     return render_template("2-index.html")
 
--- test.py
+++ test.py
@@ -2,3 +2,5 @@ list1 = {'a': 'A', 'b': 'B', 'c': 'C'}
 
 key = max(list(list1.keys()))
 print(key)
+import pytz
+print(pytz.timezone("US/Centra"))
